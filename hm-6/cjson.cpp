193143d202b67f1f1907be42e7abd9c01eacbe6f
mashfeii-laptop <evil-corporaty@yandex.ru>
2024-07-18 02:04:47 +0300

    hm-6: final build

--- hm-6/cjson.cpp
+++ hm-6/cjson.cpp
@@ -1,65 +1,83 @@
-#include <python3.10/Python.h>
+#include <python3.12/Python.h>
 
 #include <cstring>
 #include <iostream>
+#include <string>
+#include "string_utils.hpp"
 
-PyObject* construct_dict(const std::string& json_string) {
+static PyObject* parse_pair(PyObject* dict, std::string& pair) {
+  trim(pair);
+
+  PyObject* key = nullptr;
+  std::string key_string = pair.substr(1, pair.find('"', 1) - 1);
+  if (!(key = Py_BuildValue("s", key_string.data()))) {
+    printf("ERROR: Failed to build string value\n");
+    return nullptr;
+  }
+
+  PyObject* value = nullptr;
+  std::string value_string = pair.substr(pair.find(':') + 1);
+
+  if (size_t n = value_string.find('"'); n == std::string::npos) {
+    int ivalue = std::stoi(value_string);
+
+    if (!(value = Py_BuildValue("i", ivalue))) {
+      printf("ERROR: Failed to build integer value\n");
+      return nullptr;
+    }
+  } else {
+    value_string = value_string.substr(n + 1, value_string.size() - n - 2);
+
+    if (!(value = Py_BuildValue("s", value_string.data()))) {
+      printf("ERROR: Failed to build integer value\n");
+      return nullptr;
+    }
+  }
+
+  if (PyDict_SetItem(dict, key, value) < 0) {
+    printf("ERROR: Failed to set item\n");
+    return nullptr;
+  }
+
+  return dict;
+}
+static PyObject* construct_dict(PyObject* dict,
+                                std::string current_string,
+                                size_t start_index = 0) {
+  std::string pair;
+  size_t n = current_string.find(',');
+  // case 1: we got the next json entity
+  if (n != std::string::npos) {
+    pair = current_string.substr(start_index, n - start_index);
+    current_string = current_string.substr(n + 1);
+
+    // case 2: we got the last entity
+  } else if (current_string.find('}') != std::string::npos) {
+    pair = current_string.substr(0, current_string.size() - 1);
+    // case 3: unexpected syntax
+  } else {
+    std::cerr << "Could not construct the Dict Object, went to infinite loop" << std::endl;
+    return nullptr;
+  }
+
+  if (!parse_pair(dict, pair)) {
+    std::cerr << "Unable to convert JSON entity" << std::endl;
+    return nullptr;
+  }
+
+  //                         base case : recursive case
+  return (n == std::string::npos) ? dict : construct_dict(dict, current_string);
+}
+static PyObject* create_dict(const std::string& json_string) {
   PyObject* dict = nullptr;
   if (!(dict = PyDict_New())) {
     printf("ERROR: Failed to create Dict Object\n");
     return nullptr;
   }
 
-  size_t prev_position = 1;
-  std::string temp = json_string;
-  for (;;) {
-    std::string pair;
-    if (size_t n = temp.find(','); n != std::string::npos) {
-      pair = temp.substr(prev_position, n - 1);
-      temp = temp.substr(n + 1);
-    } else if (temp.find('}') != std::string::npos) {
-      pair = temp.substr(prev_position, temp.size() - prev_position - 1);
-      temp.clear();
-    } else {
-      break;
-    }
-
-    PyObject* key = nullptr;
-    std::string key_string = pair.substr(1, pair.find('"', 1) - 1);
-    if (!(key = Py_BuildValue("s", key_string.data()))) {
-      printf("ERROR: Failed to build string value\n");
-      return nullptr;
-    }
-
-    PyObject* value = nullptr;
-    std::string value_string = pair.substr(pair.find(':') + 1);
-
-    if (size_t n = value_string.find('"'); n == std::string::npos) {
-      int ivalue = std::stoi(value_string);
-
-      if (!(value = Py_BuildValue("i", ivalue))) {
-        printf("ERROR: Failed to build integer value\n");
-        return nullptr;
-      }
-    } else {
-      value_string = value_string.substr(n + 1, value_string.size() - n - 2);
-
-      if (!(value = Py_BuildValue("s", value_string.data()))) {
-        printf("ERROR: Failed to build integer value\n");
-        return nullptr;
-      }
-    }
-
-    if (PyDict_SetItem(dict, key, value) < 0) {
-      printf("ERROR: Failed to set item\n");
-      return nullptr;
-    }
-  }
-
-  return dict;
+  return construct_dict(dict, json_string, 1);
 }
-
-PyObject* cjson_loads(PyObject* self, PyObject* args) {
+static PyObject* cjson_loads(PyObject* self, PyObject* args) {
   char* buffer;
 
   if (!PyArg_ParseTuple(args, "s", &buffer)) {
@@ -69,10 +87,54 @@ PyObject* cjson_loads(PyObject* self, PyObject* args) {
 
   std::string json_string{buffer};
 
-  return construct_dict(json_string);
+  return create_dict(json_string);
 };
 
-PyObject* cjson_dumps(PyObject* self, PyObject* args) { return nullptr; };
+static PyObject* cjson_dumps(PyObject* self, PyObject* args) {
+  PyObject* dict;
+  std::string res = "{";
+
+  if (!PyArg_ParseTuple(args, "O!", &PyDict_Type, &dict)) {
+    std::cerr << "Failed to parse argument" << std::endl;
+    return nullptr;
+  }
+
+  PyObject* key;
+  PyObject* value;
+  Py_ssize_t pos = 0;
+  bool first = true;
+
+  while (PyDict_Next(dict, &pos, &key, &value)) {
+    std::string key_string{PyUnicode_AsUTF8(key)};
+    if (key_string.empty()) {
+      std::cerr << "Unable to convert key to the string" << std::endl;
+      return nullptr;
+    }
+
+    trim(key_string);
+
+    if (first) {
+      res += '\"' + key_string + "\": ";
+      first = false;
+    } else {
+      res += ", \"" + key_string + "\": ";
+    }
+
+    if (PyFloat_Check(value)) {
+      res += std::to_string(PyFloat_AsDouble(value));
+    } else if (PyLong_Check(value)) {
+      res += std::to_string(PyLong_AsLong(value));
+    } else if (PyUnicode_AsUTF8String(value)) {
+      res += '\"' + std::string{PyUnicode_AsUTF8(value)} + '\"';
+    } else {
+      std::cerr << "Unable to parse value in item" << std::endl;
+      return nullptr;
+    }
+  }
+  res += "}";
+
+  return Py_BuildValue("s", res.c_str());
+};
 
 static PyMethodDef cjson_functions[] = {
     {"loads", cjson_loads, METH_VARARGS, ""},
@@ -81,7 +143,8 @@ static PyMethodDef cjson_functions[] = {
 };
 
 static PyModuleDef module = {PyModuleDef_HEAD_INIT, "cjson",
-                             "Module with serveral methods to work with JSON",
-                             -1, cjson_functions};
+                             "Module with serveral methods to work with JSON", -1, cjson_functions};
 
-PyMODINIT_FUNC PyInit_cjson() { return PyModule_Create(&module); }
+PyMODINIT_FUNC PyInit_cjson() {
+  return PyModule_Create(&module);
+}
--- hm-6/main.py
+++ hm-6/main.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python3
 
+from random import randint
 import cjson
 import ujson
 import json
+from faker import Faker
+from faker.providers import DynamicProvider
+fake = Faker()
+
+types = DynamicProvider(
+    provider_name="types",
+    elements=["pyint", "pystr"],
+)
+fake.add_provider(types)
 
 
-def main():
-    json_str = '{"hello":10, "world":"value", "something":10}'
+def test_dumps_with_single_dict():
+    test_dict = {}
+    for i in range(1000):
+        test_dict[fake.pystr(min_chars=1, max_chars=20000)] = fake.random_element(
+            elements=(fake.random_int(min=0, max=250000),
+                      fake.pystr(min_chars=1, max_chars=250000)))
+    assert (cjson.dumps(test_dict)) == json.dumps(test_dict)
 
-    json_doc = json.loads(json_str)  # default Python library
-    cjson_doc = cjson.loads(json_str)  # C API module
-    ujson_doc = ujson.loads(json_str)  # PIP library
-    print("standard=", json_doc, "\nc_api=", cjson_doc, sep='')
-    assert json_doc == cjson_doc
+
+def test_dumps_with_several_dicts():
+    for i in range(1000):
+        test_dict = fake.pydict(nb_elements=100,
+                                variable_nb_elements=False,
+                                value_types=[int, str])
+        assert (cjson.dumps(test_dict)) == json.dumps(test_dict)
 
 
-if __name__ == "__main__":
-    main()
+def test_loads():
+    for i in range(1000):
+        structure = fake.pydict(nb_elements=randint(
+            4, 50), value_types=[fake.types, fake.types])
+        json_str = fake.json(data_columns=structure, num_rows=1)
+        assert (cjson.loads(json_str)) == ujson.loads(json_str)
+        assert (cjson.loads(json_str)) == json.loads(json_str)
